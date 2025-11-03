aadb60a33da507bd836a8f6eb2c8d32228a634e8
Ramanand mandal <ramanandmandal759@gamil.com>
2025-11-03 16:42:06 +0545

    first commit

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://riseup-connect.onrender.com/api';
 
 // Create axios instance
 const api = axios.create({
